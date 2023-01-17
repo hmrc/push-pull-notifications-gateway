77fa8b0c4ca3c0078389a81d98611501526f538c
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2023-01-17 07:23:53 +0000

    API-5863: scalastyle config changes, plugin version updates and add scala fix,fmt & style to run all tests

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export SBT_OPTS="-XX:MaxMetaspaceSize=1G"
-sbt clean compile coverage test it:test scalastyle coverageReport
+sbt clean compile coverage scalastyle scalafmtAll scalafixAll test it:test scalastyle coverageReport
 unset SBT_OPTS
